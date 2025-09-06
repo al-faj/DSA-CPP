46e8b87610651c9e6b7c8837f13a1b0c5a7c3b19
al-faj <142720630+al-faj@users.noreply.github.com>
2025-09-06 07:11:08 -0700

    Create invided-pyramid.cpp

---
+++ invided-pyramid.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // int num=8;
+    int n=4;
+    //invated pyramid
+    for(int row=0; row<n; row++){
+        for(int col=0; col<n-row; col++){
+            cout << "* ";
+            //
+        }
+        //full space pyramid
+        for(int col=0; col<n*row+1; col++){
+            cout << " ";
+        }
+        for(int col=0; col<n-row; col++){
+            cout << "* ";
+            //
+        }
+        cout <<endl;
+    }
+     for(int row=0; row<n; row++){
+        for(int col=0; col<row+1; col++){
+            cout << "* ";
+            //
+        }
+        //full space pyramid
+        for(int col=0; col<2*n-2*row-1; col++){
+            cout << "  ";
+        }
+        for(int col=0; col<row+1; col++){
+            cout << "* ";
+            //
+        }
+        cout <<endl;
+    }
+}
